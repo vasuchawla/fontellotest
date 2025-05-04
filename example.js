f38d0086ded7984010aa3f1244c700c9ddb75b38
Vasu Chawla <vchawla26@gmail.com>
2025-05-04 23:52:58 +0530

    Create example.js

---
+++ example.js
@@ -0,0 +1,8 @@
+python3 componentusage.py /Users/vasu/Downloads/newplaygroup    --auto-discover --html report.html --csv report.csv
+
+
+
+
+python3 componentusage.py /Users/vasu/Downloads/newplaygroup    --components CustomButton CustomText --html report.html --csv report.csv
+
+
